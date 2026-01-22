ec2996670dfdefc67b50095fa6f83403e3ffecde
Miguel Torres <mt931109@gmail.com>
2026-01-22 17:40:42 -0500

    rutas intento 2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/',
+    base: process.env.GITHUB_PAGES === 'true' ? '/Portafolio-React/' : '/',
+    build: {
+        outDir: 'dist',
+        sourcemap: false,
+    },
 })
